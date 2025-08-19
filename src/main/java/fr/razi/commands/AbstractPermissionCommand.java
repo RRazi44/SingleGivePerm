5112056ce6cb278864885840c7731b20d883b77c
Razi <razi.azizi.decembre2006@gmail.com>
2025-08-19 20:44:45 +0200

    Add the giveperm and the removeperm

---
+++ src/main/java/fr/razi/commands/AbstractPermissionCommand.java
@@ -0,0 +1,48 @@
+package fr.razi.commands;
+
+import org.bukkit.Bukkit;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public abstract class AbstractPermissionCommand implements CommandExecutor {
+    private final boolean give;
+
+    protected AbstractPermissionCommand(boolean give) {
+        this.give = give;
+    }
+
+    @Override
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args){
+
+        if(!hasPerm(sender)) return true;
+
+        if (args.length < 2) {
+            sender.sendMessage("§cUsage: /" + label + " <player> <permission>");
+            return true;
+        }
+
+        Player target = Bukkit.getPlayer(args[0]);
+
+        if (target == null) {
+            sender.sendMessage("§cPlayer not found!");
+            return true;
+        }
+
+        String permission = args[1];
+        applyPermission(target, permission, give);
+        sender.sendMessage("§aPermission " + (give ? "given" : "removed") + " to " + target.getName());
+
+        return true;
+    }
+
+    protected abstract void applyPermission(Player targetPlayer, String permission, boolean give);
+
+    protected boolean hasPerm(CommandSender sender){
+        return sender.isOp() || sender.hasPermission("singlegiveperm.commands.giveperm");
+    }
+
+    public boolean isGive(){return give;}
+
+}
